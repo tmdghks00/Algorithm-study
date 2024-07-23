73c45fabce784be19764fe55950bf0f119a45683
Seunghwan830 <wxtq22@naver.com>
2024-07-23 12:45:08 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 300 ms, Memory: 25460 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        
+        Set<String> set = new HashSet<>();
+        //  String의 중복여부를 확인하기 위한 HashSet
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            if (s.equals("ENTER")) {
+                set.clear(); // ENTER 가 나오면 HashSet 초기화
+            } else {
+                if (set.contains(s)) continue;
+                set.add(s); // set에 s가 없으면 s를 추가하고 횟수 증가시킴
+               cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
+// 
